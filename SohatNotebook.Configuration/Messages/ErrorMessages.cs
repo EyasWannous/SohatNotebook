66ba00ba5f30648114307652aa0ff39f7c030ec5
Eyas Wannous <eyaswannous@gmail.com>
2024-04-24 18:33:45 +0300

    Add 'Configuration'

---
+++ SohatNotebook.Configuration/Messages/ErrorMessages.cs
@@ -0,0 +1,24 @@
+namespace SohatNotebook.Configuration.Messages;
+
+public static class ErrorMessages
+{
+    public static class Generic
+    {
+        public const string ObjectNotFound = "Object Not Found";
+        public const string InvalidRequest = "Invalid request";
+        public const string UnableToProcess = "Unable to process request";
+        public const string InvalidPayload = "Invalid payload";
+        public const string SometingWentWrong = "Something went wrong, please try again later";
+        public const string TypeBadRequest = "Bad Request";
+    }
+
+
+    public static class Profile
+    {
+        public const string UserNotFound = "User not found";
+    }
+    public static class Users
+    {
+        public const string UserNotFound = "User not found";
+    }
+}
